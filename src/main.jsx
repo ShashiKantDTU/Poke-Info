9846fb40f91f4e29cafacd30858bafed7f0b5507
Shashi Kant <shashikant_23en516@dtu.ac.in>
2025-02-04 02:21:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { init } from '@vercel/speed-insights';
-
-init({
-    projectName: 'poke-info', // Replace with your actual project name
-});
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
+        <SpeedInsights/>
     </StrictMode>
 
 )
